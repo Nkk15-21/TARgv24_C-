cc4aeeb8b16f3542efc05f8156535c50f2613c0b
Nkk15-21 <nkk.sith@gmail.com>
2025-04-16 17:13:45 +0300

    9/1

--- FunctionClass_2osa.cs
+++ FunctionClass_2osa.cs
@@ -8,6 +8,8 @@ namespace TARgv24_C
 {
     internal class FunctionClass_2osa
     {
+        // ----------------------------------------------------------------
+
         public static List<string> Sõnad()
         {
             List<string> sonad = new List<string>();
@@ -18,6 +20,8 @@ namespace TARgv24_C
             }
             return sonad;
         }
+
+        // ----------------------------------------------------------------
 
         public static isik[] isikud(int k, string[] nimed, string[] aadressid)
         {
@@ -39,7 +43,56 @@ namespace TARgv24_C
                 
         }
 
+        // ---------------------------9/1-------------------------------------
 
+        public static List<int> ArvudeRuudud() // создаём список для чисел
+        {
+            int[] arvud = { 2, 4, 6, 8, 10, 12 }; //библиотека чисел
+            List<int> ruudud = new List<int>(); //создаём список квадратов чисел и новый список куда будем писать
+
+            for (int i = 0; i < arvud.Length; i++) 
+                // начинаем с нуля,  не меньше того, что в массиве arvud, + 1 каждый раз
+            {
+                ruudud.Add(arvud[i] * arvud[i]);// добавляем в список квадратов(число[i] в квадрат)
+            }
+
+            return ruudud;
+        }
+
+        // ----------------------------9/2------------------------------------
+
+        public static List<int> Kahekordsed() // создаём список для чисел
+        {
+            int[] arvud = { 2, 4, 6, 8, 10, 12 }; //массив чисел
+            List<int> topelt = new List<int>(); // создаём список для результатов 
+
+            foreach (int arv in arvud) // берём отдельное число, а не весь массив
+            {
+                topelt.Add(arv * 2); //доюавляем в список topelt(умноженое число а)
+            }
+
+            return topelt;
+        }
+
+        // ------------------------------9/3----------------------------------
+
+        public static int KolmegaJaguvad() // создаём список для чисел
+        {
+            int[] arvud = { 2, 4, 6, 8, 10, 12 }; //массив чисел
+            int loendur = 0; // счётчик
+            int i = 0;
+
+            while (i < arvud.Length) // Пока i меньше длины массива — продолжаем.
+            {
+                if (arvud[i] % 3 == 0) // проверка, делится ли число на 3 без остатка.
+                {
+                    loendur++; //если да, то добавляем к счётчику +1
+                }
+                i++; // обязательно увеличиваем i, иначе цикл будет бесконечным).
+            }
+
+            return loendur; //возвращаем, сколько чисел делится на 3.
+        }
 
     }
 }
--- MainClass.cs
+++ MainClass.cs
@@ -11,76 +11,99 @@ namespace TARgv24_C
         public static void Main(string[] args)
         {
 
-            //II.osa
+            //9 задание
 
-            List<string> sonad = FunctionClass_2osa.Sõnad();
-            foreach (var item in sonad)
-            {
-                Console.WriteLine(item);
-            }
+            //9-1
+            Console.WriteLine("Ruutude arvutamine (for):");
+            List<int> ruudud = FunctionClass_2osa.ArvudeRuudud();
+            foreach (var r in ruudud)
+                Console.WriteLine(r);
 
-            Console.WriteLine("---------------------------------");
+            //9-2
+            Console.WriteLine("\nKahekordsed väärtused (foreach):");
+            List<int> topelt = FunctionClass_2osa.Kahekordsed();
+            foreach (var t in topelt)
+                Console.WriteLine(t);
 
-            //-----------------------------------------------------------------
-
-            isik isik1 = new isik("Juku", 65, "12345678987", "Tallinn");
-            isik1.PrindiInfo();
-            isik isik2 = new isik();
-            isik2.Nimi = "Mari";
-            isik2.Aadress = "Tartu";
-            isik2.Isikukood = "98765432100";
-            isik2.Sugu = Sugu.Naine;
-            isik2.PrindiInfo();
-
-            //--------------------------------------------------------------------
-
-            int i;
-            string[] nimed = new string[10] { "a", "b", "c", "d", "e", "f", "g", "h", "j", "k" };
-            string[] aadressid = new string[10] { "Tallinn", "Tartu", "Pärnu", "Narva", "Kohtla", "Jõhvi", "Ahtme", "Rakvere", "Moskva", "Saint-Petersburg" };
-            Console.WriteLine("----------for++Massiv-------");
-            isik[] isikud = FunctionClass_2osa.isikud(nimed.Length, nimed, aadressid);
-           
-            for (i = 0; i < nimed.Length; i++)
-            {
-                isikud[i].PrindiInfo();
-            }
-
-            //-------------------------------------------------------------------------
-
-            Console.WriteLine("----------for--List-------");
-            List<isik> isikud2 = new List<isik>();
-            for (int j = i-1; j > -1; j--)
-            {
-                Console.WriteLine(j);
-                isik isik = new isik
-                {
-                    Nimi = nimed[j],
-                    Vanus = j + 10,
-                    Isikukood = "12345678950",
-                    Aadress = aadressid[j]
-                };
-                isikud2.Add(isik);
-            }
-            foreach (var isik in isikud2)
-            {
-                isik.PrindiInfo();
-            }
+            //9-3
+            Console.WriteLine("\nKolmega jaguvad arvud (while):");
+            int jaguvateArv = FunctionClass_2osa.KolmegaJaguvad();
+            Console.WriteLine($"Kokku: {jaguvateArv}");
 
 
 
-            Console.WriteLine("------while------");
-            while (i>=0)
-            {
-                Console.WriteLine(i);
-                i--;
-            }
-            for (i=0; i<10; i++)
-            {
-                isikud[i].PrindiInfo();
-            }
 
-            Console.WriteLine("-----------for-----------");
-            
+
+            ////II.osa
+
+            //List<string> sonad = FunctionClass_2osa.Sõnad();
+            //foreach (var item in sonad)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine("---------------------------------");
+
+            ////-----------------------------------------------------------------
+
+            //isik isik1 = new isik("Juku", 65, "12345678987", "Tallinn");
+            //isik1.PrindiInfo();
+            //isik isik2 = new isik();
+            //isik2.Nimi = "Mari";
+            //isik2.Aadress = "Tartu";
+            //isik2.Isikukood = "98765432100";
+            //isik2.Sugu = Sugu.Naine;
+            //isik2.PrindiInfo();
+
+            ////--------------------------------------------------------------------
+
+            //int i;
+            //string[] nimed = new string[10] { "a", "b", "c", "d", "e", "f", "g", "h", "j", "k" };
+            //string[] aadressid = new string[10] { "Tallinn", "Tartu", "Pärnu", "Narva", "Kohtla", "Jõhvi", "Ahtme", "Rakvere", "Moskva", "Saint-Petersburg" };
+            //Console.WriteLine("----------for++Massiv-------");
+            //isik[] isikud = FunctionClass_2osa.isikud(nimed.Length, nimed, aadressid);
+
+            //for (i = 0; i < nimed.Length; i++)
+            //{
+            //    isikud[i].PrindiInfo();
+            //}
+
+            ////-------------------------------------------------------------------------
+
+            //Console.WriteLine("----------for--List-------");
+            //List<isik> isikud2 = new List<isik>();
+            //for (int j = i-1; j > -1; j--)
+            //{
+            //    Console.WriteLine(j);
+            //    isik isik = new isik
+            //    {
+            //        Nimi = nimed[j],
+            //        Vanus = j + 10,
+            //        Isikukood = "12345678950",
+            //        Aadress = aadressid[j]
+            //    };
+            //    isikud2.Add(isik);
+            //}
+            //foreach (var isik in isikud2)
+            //{
+            //    isik.PrindiInfo();
+            //}
+
+
+
+            //Console.WriteLine("------while------");
+            //while (i>=0)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+            //for (i=0; i<10; i++)
+            //{
+            //    isikud[i].PrindiInfo();
+            //}
+
+            //Console.WriteLine("-----------for-----------");
+
 
 
 
